574f988aa99013a62975a3d20c44db78aa1eca0d
Nidaiq <nida.iqbal@gmail.com>
2021-07-28 10:44:55 -0400

    taking db out again

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -19,16 +19,6 @@ import nltk.data
 #db_string = f"postgresql://postgres:{postgrespwd}@localhost:5432/FakeNewsDetector"
 #engine = create_engine(db_string)
 
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-from .models import Pet
-
 
 app = Flask(__name__)
 @app.route('/')
