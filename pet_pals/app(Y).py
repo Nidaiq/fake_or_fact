5dfc7ff699f782873e8d3e7842fe53ffa242514b
Nidaiq <nida.iqbal@gmail.com>
2021-07-27 23:09:16 -0400

    adding file

--- pet_pals/app(Y).py
+++ pet_pals/app(Y).py
@@ -1,7 +1,6 @@
-# import necessary libraries
-import os
 import pandas as pd
 from sqlalchemy import create_engine
+from flask import Flask, render_template, redirect, request, jsonify
 from config import postgrespwd
 import re
 import string
@@ -9,41 +8,20 @@ import numpy as np
 from tqdm import tqdm
 import time
 from collections import Counter
+# from pathlib import Path
+# import matplotlib.pyplot as plt
+# from sklearn.linear_model import LinearRegression
 import nltk.data
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+# from nltk.tokenize import sent_tokenize, word_tokenize
+# from nltk.corpus import stopwords
 
-#################################################
-# Flask Setup
-#################################################
+db_string = f"postgresql://postgres:{postgrespwd}@localhost:5432/FakeNewsDetector"
+engine = create_engine(db_string)
+
 app = Flask(__name__)
-
-#################################################
-# Database Setup
-#################################################
-
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-from .models import Pet
-
-
-# create route that renders index.html template
-@app.route("/")
+@app.route('/')
 def welcome():
     return render_template("index.html")
-
-
-# Query the database and send the jsonified results
 
 @app.route('/verify/<title>/<text>', methods=['GET', 'POST'])
 def verifyArticle(title,text):
@@ -84,4 +62,4 @@ def verifyArticle(title,text):
     # return(articleInfo)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
