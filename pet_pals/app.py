3b3549976db5eed4bdb1d7486f31e78a41757b69
Nidaiq <nida.iqbal@gmail.com>
2021-07-31 10:24:38 -0400

    reverting back to app.py that worked

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -13,10 +13,8 @@ from collections import Counter
 # import matplotlib.pyplot as plt
 # from sklearn.linear_model import LinearRegression
 import nltk.data
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-import pickle
-from sklearn.preprocessing import StandardScaler
+# from nltk.tokenize import sent_tokenize, word_tokenize
+# from nltk.corpus import stopwords
 
 #db_string = f"postgresql://postgres:{postgrespwd}@localhost:5432/FakeNewsDetector"
 #engine = create_engine(db_string)
@@ -27,13 +25,11 @@ app = Flask(__name__)
 def welcome():
     return render_template("index.html")
 
-@app.route('/verify/<subject>/<title>/<text>', methods=['GET', 'POST'])
-def verifyArticle(subject,title,text):
-    articleInfo = {'subject':subject,'title':title,'text':text}
+@app.route('/verify/<title>/<text>', methods=['GET', 'POST'])
+def verifyArticle(title,text):
+    articleInfo = {'title':title,'text':text}
     article_df = pd.DataFrame(articleInfo, index=[0])
     article_df['article'] = article_df['title']+" "+article_df['text']
-    article_df['title'] = article_df['title'].str.replace('U.S.', 'USA').str.replace('U.S', 'USA').str.replace(' US ', ' USA ')
-    article_df['text'] = article_df['text'].str.replace('U.S.', 'USA').str.replace('U.S', 'USA').str.replace(' US ', ' USA ')
     article_df['article'] = article_df['article'].str.replace('U.S.', 'USA').str.replace('U.S', 'USA').str.replace(' US ', ' USA ')
     @np.vectorize
     def wordpre(x):
@@ -48,19 +44,8 @@ def verifyArticle(subject,title,text):
         x = re.sub('\n', '', x)
         x = re.sub('\w*\d\w*', '', x)
         return x
-    article_df['title']= article_df['title'].apply(wordpre)
-    article_df['text']= article_df['text'].apply(wordpre)
     article_df['article']= article_df['article'].apply(wordpre)
-    stop = stopwords.words('english')
-    article_df['title']= article_df['title'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
-    article_df['text']= article_df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
-    article_df['title_count'] = article_df['title'].apply(len)-1
-    article_df['text_count'] = article_df['text'].apply(len)-1
-    article_df['title_tokens'] = article_df['title'].apply(word_tokenize)
-    article_df['text_tokens'] = article_df['text'].apply(word_tokenize)
-    article_df['title_tokenized_count'] = article_df['title_tokens'].apply(len)
-    article_df['text_tokenized_count'] = article_df['text_tokens'].apply(len)
-    subject_dummies = pd.get_dummies(article_df['subject'])
+    nltk.download('averaged_perceptron_tagger')
     ndf = pd.DataFrame()
     text = article_df['article'].apply(nltk.tokenize.WhitespaceTokenizer().tokenize)
     for i in text: 
@@ -69,29 +54,14 @@ def verifyArticle(subject,title,text):
         S = pd.Series([C])
         N = pd.DataFrame.from_records(S, columns = S.sum().keys())
         ndf = pd.concat([ndf, N], ignore_index=True, sort=False)
+    ndf = ndf.fillna(0)
     ndf["sum"] = ndf.sum(axis=1)
-    ndf = ndf.div(ndf['sum'], axis=0) *100
-    features_df = pd.concat([
-    article_df.drop(['subject','title','text','article','title_tokens','text_tokens'],axis=1),
-    subject_dummies,
-    ndf.drop('sum',axis=1)
-    ], axis=1)
-    reqd_features = ['title_count', 'text_count', 'title_tokenized_count',
-        'text_tokenized_count', 'US News', 'World News', 'JJ', 'NN', 'VBZ',
-        'RP', 'VBG', 'VBP', 'DT', 'RB', 'VB', 'CC', 'PRP', 'IN', 'VBD', 'TO',
-        'PRP$', 'NNS', 'JJS', 'CD', 'JJR', 'RBR', 'VBN', 'MD', 'WP', 'FW',
-        'NNP', 'WRB', 'WDT', 'PDT', 'EX', 'RBS', 'NNPS', 'UH', 'WP$', 'POS']
-    for i in reqd_features:
-        if(i not in list(features_df.columns)):
-            features_df[i]=0
-    features = features_df.iloc[0]
-    root = os.path.dirname(os.path.abspath(__file__))  
-    ml_file_path = os.path.join(root, 'static/svm_model.sav')
-    model = pickle.load(open(ml_file_path, 'rb'))
-    result = model.predict(features)[0]
-    
-    return render_template("results.html",results=result)
-
+    normalized_df = ndf.div(ndf['sum'], axis=0) *100
+    nlp_output_df = pd.concat([article_df,normalized_df], axis=1)
+    nlp_output_df = nlp_output_df.drop(['text','title','article'], axis = 1)
+    # Output from ML is returned in next line
+    return render_template("results.html",results=nlp_output_df.to_html())
+    # return(articleInfo)
 
 
 if __name__ == "__main__":
