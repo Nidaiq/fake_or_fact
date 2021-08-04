5265d341f7ea396ae3bf227e8289d47821359f7f
Nidaiq <nida.iqbal@gmail.com>
2021-08-04 01:08:06 -0400

    adding new app.py

--- pet_pals/app.py
+++ pet_pals/app.py
@@ -11,29 +11,29 @@ app = Flask(__name__)
 def welcome():
     return render_template("index.html")
 
-@app.route('/verdict/<subject>/<title>/<text>', methods=['GET', 'POST'])
-def classifyArticle(subject,title,text):
-    
+@app.route('/verdict', methods=['GET', 'POST'])
+def classifyArticle():
     # Read Test Data into a Dataframe
-    articleInfo = {'subject':subject,'title':title,'text':text}
+    articleInfo = {
+        'title':request.form['title'],
+        'text':request.form['text']
+    }
     article_df = pd.DataFrame(articleInfo, index=[0])
-
     # Load ML Dependencies
     root = os.path.dirname(os.path.abspath(__file__)) 
     cv_file_path = os.path.join(root,'static/machineLearning/cv.sav')
     nb_file_path = os.path.join(root,'static/machineLearning/nb_model.sav')
     countVectorizer = pickle.load(open(cv_file_path,'rb'))
     naiveBayes = pickle.load(open(nb_file_path,'rb'))
-
     # Predict Outcome
     cv_text = countVectorizer.transform(article_df['text'])
     outcome = naiveBayes.predict(cv_text)[0]
+    # Return Result
     if (outcome == 0):
         classification = 'fact'
     elif (outcome == 1):
         classification = 'fake'
-
     return render_template("results.html",results=classification)
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
